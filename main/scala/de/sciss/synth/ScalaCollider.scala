9c61fddb7d318f85b962890ebdfe004897cfd2c5
Sciss <contact@sciss.de>
2011-06-24 21:40:06 +0100

    ...

--- main/scala/de/sciss/synth/ScalaCollider.scala
+++ main/scala/de/sciss/synth/ScalaCollider.scala
@@ -32,7 +32,7 @@ object ScalaCollider {
    val name          = "ScalaCollider"
    val version       = 0.30
    val copyright     = "(C)opyright 2008-2011 Hanns Holger Rutz"
-   var isSnapshot    = true
+   val isSnapshot    = true
 
    def versionString = {
       val s = (version + 0.001).toString.substring( 0, 4 )
