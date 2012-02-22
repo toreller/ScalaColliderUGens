f54fbea31d754d4e58be362cb5006a0c0fa44342
Sciss <contact@sciss.de>
2012-02-22 22:28:02 +0000

    make it 0.33 final

--- main/scala/de/sciss/synth/ScalaCollider.scala
+++ main/scala/de/sciss/synth/ScalaCollider.scala
@@ -29,7 +29,7 @@ object ScalaCollider {
    val name          = "ScalaCollider"
    val version       = 0.33
    val copyright     = "(C)opyright 2008-2012 Hanns Holger Rutz"
-   val isSnapshot    = true
+   val isSnapshot    = false
 
    def versionString = {
       val s = (version + 0.001).toString.substring( 0, 4 )
