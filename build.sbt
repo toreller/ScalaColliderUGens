b4d27b44d70794d3186cd713ff086bb657f1f757
Sciss <contact@sciss.de>
2013-02-07 15:09:14 +0100

    multi project build file. basic structures seems right. we'll have the xml's published as a jar, then the api, then core which will be filled with sources produced by gen. probably we'll have to add back RichNumber etc. to core because that stuff will be used by default values from the spec

--- build.sbt
+++ build.sbt
@@ -1,44 +1,30 @@
 name := "ScalaColliderUGens"
 
-version := "1.0.1"
+version in ThisBuild := "1.0.1"
 
-organization := "de.sciss"
+organization in ThisBuild := "de.sciss"
 
-scalaVersion := "2.9.2"
+scalaVersion in ThisBuild := "2.9.2"
 
-description := "Generates UGen source code files for ScalaCollider"
+description in ThisBuild := "Generates UGen source code files for ScalaCollider"
 
-homepage := Some( url( "https://github.com/Sciss/ScalaColliderUGens" ))
+homepage in ThisBuild := Some(url("https://github.com/Sciss/ScalaColliderUGens"))
 
-licenses := Seq( "GPL v2+" -> url( "http://www.gnu.org/licenses/gpl-2.0.txt" ))
+licenses in ThisBuild := Seq("GPL v2+" -> url("http://www.gnu.org/licenses/gpl-2.0.txt"))
 
-libraryDependencies ++= Seq(
-   "com.github.scopt" %% "scopt" % "2.1.0",
-   "org.scala-refactoring" % "org.scala-refactoring.library" % "0.4.1"
-)
+retrieveManaged in ThisBuild := true
 
-libraryDependencies <+= scalaVersion { sv =>
-   "org.scala-lang" % "scala-compiler" % sv
-}
-
-//   "org.scala-refactoring" % "org.scala-refactoring.library" % "0.4.1"
-//    from
-//        "https://oss.sonatype.org/content/repositories/releases/org/scala-refactoring/org.scala-refactoring_2.9.1/0.4.1/org.scala-refactoring_2.9.1-0.4.1.jar"
-////        "http://scala-tools.org/repo-snapshots/org/scala-refactoring/org.scala-refactoring_2.9.1/0.3.0-SNAPSHOT/org.scala-refactoring_2.9.1-0.3.0-20110920.122544-11.jar"
-
-retrieveManaged := true
-
-scalacOptions ++= Seq( "-deprecation", "-unchecked" )
+scalacOptions in ThisBuild ++= Seq("-deprecation", "-unchecked")
 
 // ---- build info ----
-
-buildInfoSettings
-
-sourceGenerators in Compile <+= buildInfo
-
-buildInfoKeys := Seq( name, organization, version, scalaVersion, description,
-   BuildInfoKey.map( homepage ) { case (k, opt) => k -> opt.get },
-   BuildInfoKey.map( licenses ) { case (_, Seq( (lic, _) )) => "license" -> lic }
-)
-
-buildInfoPackage := "de.sciss.synth.ugen"
+//  
+// buildInfoSettings
+// 
+// sourceGenerators in Compile <+= buildInfo
+// 
+// buildInfoKeys := Seq( name, organization, version, scalaVersion, description,
+//    BuildInfoKey.map( homepage ) { case (k, opt) => k -> opt.get },
+//    BuildInfoKey.map( licenses ) { case (_, Seq( (lic, _) )) => "license" -> lic }
+// )
+// 
+// buildInfoPackage := "de.sciss.synth.ugen"
---
+++ project/Build.scala
@@ -0,0 +1,75 @@
+import sbt._
+import Keys._
+import sbtbuildinfo.Plugin._
+
+object Build extends sbt.Build {
+  lazy val root: Project = Project(
+    id = "scalacolliderugens",
+    base = file("."),
+    aggregate = Seq(spec, api, gen, core)
+  )
+
+  // taking inspiration from http://stackoverflow.com/questions/11509843/sbt-generate-code-using-project-defined-generator
+  val ugenGenerator = TaskKey[Seq[File]]("ugen-generate", "Generate UGen class files")
+
+  lazy val spec = Project(
+    id = "scalacolliderugens-spec",
+    base = file("spec"),
+    settings = Project.defaultSettings ++ buildInfoSettings ++ Seq(
+      // buildInfoSettings
+//      sourceGenerators in Compile <+= buildInfo,
+//      buildInfoKeys := Seq(name, organization, version, scalaVersion, description,
+//        BuildInfoKey.map(homepage) {
+//          case (k, opt) => k -> opt.get
+//        },
+//        BuildInfoKey.map(licenses) {
+//          case (_, Seq((lic, _))) => "license" -> lic
+//        }
+//      ),
+//      buildInfoPackage := "de.sciss.lucre.stm"
+    )
+  )
+
+  lazy val api = Project(
+    id = "scalacolliderugens-api",
+    base = file("api"),
+//    dependencies = Seq(xml),
+    settings = Project.defaultSettings ++ Seq(
+//      resolvers += "Oracle Repository" at "http://download.oracle.com/maven", // required for sleepycat
+//      libraryDependencies += "com.sleepycat" % "je" % "5.0.58" // = Berkeley DB Java Edition
+    )
+  )
+
+  lazy val gen = Project(
+    id = "scalacolliderugens-gen",
+    base = file("gen"),
+    dependencies = Seq(spec),
+    settings = Project.defaultSettings ++ Seq(
+//      resolvers += "Oracle Repository" at "http://download.oracle.com/maven", // required for sleepycat
+      libraryDependencies ++= Seq(
+        "com.github.scopt" %% "scopt" % "2.1.0",
+        "org.scala-refactoring" % "org.scala-refactoring_2.9.1" % "0.4.1"
+      ),
+      libraryDependencies <+= scalaVersion { sv =>
+        "org.scala-lang" % "scala-compiler" % sv
+      }
+    )
+  )
+
+  lazy val core = Project(
+    id = "scalacolliderugens-core",
+    base = file("core"),
+    settings = Project.defaultSettings ++ Seq(
+      sourceGenerators in Compile <+= (ugenGenerator in Compile),
+      ugenGenerator in Compile <<=
+        (scalaSource in Compile, dependencyClasspath in Runtime in gen) map {
+          (src, cp) => runUGenGenerator(src, cp.files)
+        }
+      )
+    ).dependsOn(gen)
+
+  def runUGenGenerator(source: File, cp: Seq[File]): Seq[File] = {
+    println("AQUI " + source)
+    Nil
+  }
+}
