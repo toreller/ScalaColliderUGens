216f2e7741045a33d74e851bac32d1f9c65d1397
Sciss <contact@sciss.de>
2013-02-07 13:12:31 +0100

    `DoubleFun`

--- main/scala/de/sciss/synth/GE.scala
+++ main/scala/de/sciss/synth/GE.scala
@@ -28,7 +28,6 @@ package de.sciss.synth
 import scala.{Seq => SSeq}
 import collection.breakOut
 import collection.immutable.{ IndexedSeq => IIdxSeq }
-import ugen.{ChannelProxy, LinLin, BinaryOp, UnaryOp, MulAdd}
 
 /**
  *    The UGen graph is constructed from interconnecting graph elements (GE).
@@ -45,28 +44,21 @@ object GE {
    }
 
    implicit def fromIntSeq( xs: SSeq[ Int ]) : GE = xs match {
-      case SSeq( single ) => single: Constant
+      case SSeq( single ) => Constant(single)
       case _ => SeqImpl( xs.map( i => Constant( i.toFloat ))( breakOut ))
    }
 
    implicit def fromFloatSeq( xs: SSeq[ Float ]) : GE = xs match {
-      case SSeq( x ) => x: Constant
+      case SSeq( x ) => Constant(x)
       case _ => SeqImpl( xs.map( f => Constant( f ))( breakOut ))
    }
 
    implicit def fromDoubleSeq( xs: SSeq[ Double ]) : GE = xs match {
-      case SSeq( x ) => x: Constant
+      case SSeq( x ) => Constant(x.toFloat)
       case _ => SeqImpl( xs.map( d => Constant( d.toFloat ))( breakOut ))
    }
 
    def fromUGenIns( xs: SSeq[ UGenIn ]) : GE = SeqImpl2( xs.toIndexedSeq )
-
-//   implicit def fromSeq[ R <: Rate, G ]( x: Seq[ G ])( implicit view: G => GE[ R ]) : GE[ R ] = {
-//      x match {
-//         case Seq( single ) => single // Multi.Joint( single )
-//         case _ => GESeq[ R ]( x.map( view )( breakOut ))
-//      }
-//   }
 
    @SerialVersionUID(-5589376755121907882L) private final case class SeqImpl( elems: IIdxSeq[ GE ]) extends GE {
 def numOutputs = elems.size
@@ -83,218 +75,10 @@ def numOutputs = elems.size
       override def toString = displayName + elems.mkString( "(", ",", ")" )
    }
 
-//   object Seq {
-////      implicit def toIndexedSeq[ R <: Rate ]( g: Seq[ R ]) : IIdxSeq[ GE[ R ]] = g.elems
-////      def apply[ R <: Rate ]( elems: IIdxSeq[ GE[ R ]]) : Seq[ R ] = new SeqImpl( elems )
-//      def apply[ R <: Rate ]( xs: GE[ R ]* ) : Seq[ R ] = new SeqImpl( xs.toIndexedSeq )
-//      def apply( xs: UGenIn* ) : Seq[ Rate ] = new SeqImpl2( xs.toIndexedSeq )
-//   }
-//   sealed trait Seq[ R <: Rate ] extends GE[ R ]
-
    /**
     * Simply a trait composed of `Lazy.Expander[UGenInLike]` and `GE`
     */
    trait Lazy extends Lazy.Expander[ UGenInLike ] with GE
-   
-//   implicit def ops( g: GE ) : Ops = new Ops( g )
-   
-   final class Ops( g: GE ) {
-      def `\\`( index: Int ) = ChannelProxy( g, index )
-
-      def madd( mul: GE, add: GE ) = MulAdd( g, mul, add )
-
-//      def flatten = Flatten( g )
-//
-//      def poll: Poll = poll()
-//
-//      /**
-//       * Polls the output values of this graph element, and prints the result to the console.
-//       * This is a convenient method for wrapping this graph element in a `Poll` UGen.
-//       *
-//       * @param   trig     a signal to trigger the printing. If this is a constant, it is
-//       *    interpreted as a frequency value and an `Impulse` generator of that frequency
-//       *    is used instead.
-//       * @param   label    a string to print along with the values, in order to identify
-//       *    different polls. Using the special label `"#auto"` (default) will generated
-//       *    automatic useful labels using information from the polled graph element
-//       * @param   trigID   if greater then 0, a `"/tr"` OSC message is sent back to the client
-//       *    (similar to `SendTrig`)
-//       *
-//       * @see  [[de.sciss.synth.ugen.Poll]]
-//       */
-//      def poll( trig: GE = 10, label: Optional[ String ] = None, trigID: GE = -1 ) : Poll = {
-//         val trig1 = trig match {
-//            case Constant( freq ) => Impulse( (g.rate ?| audio) max control, freq, 0 )  // XXX good? or throw an error? should have a maxRate?
-//            case other => other
-//         }
-//         Poll( trig1.rate, trig1, g, label.getOrElse( g.displayName ), trigID  )
-//      }
-
-      import UnaryOp._
-
-      // unary ops
-      def unary_- : GE   = Neg.make( g )
-   // def bitNot : GE	         = BitNot.make( g )
-      def abs : GE       = Abs.make( g )
-   // def toFloat : GE	         = UnOp.make( 'asFloat, this )
-   // def toInteger : GE	      = UnOp.make( 'asInteger, this )
-      def ceil : GE      = Ceil.make( g )
-      def floor : GE     = Floor.make( g )
-      def frac : GE      = Frac.make( g )
-      def signum : GE    = Signum.make( g )
-      def squared : GE   = Squared.make( g )
-      def cubed : GE     = Cubed.make( g )
-      def sqrt : GE      = Sqrt.make( g )
-      def exp : GE       = Exp.make( g )
-      def reciprocal : GE= Reciprocal.make( g )
-      def midicps : GE   = Midicps.make( g )
-      def cpsmidi : GE   = Cpsmidi.make( g )
-      def midiratio : GE = Midiratio.make( g )
-      def ratiomidi : GE = Ratiomidi.make( g )
-      def dbamp : GE     = Dbamp.make( g )
-      def ampdb : GE     = Ampdb.make( g )
-      def octcps : GE    = Octcps.make( g )
-      def cpsoct : GE    = Cpsoct.make( g )
-      def log : GE       = Log.make( g )
-      def log2 : GE      = Log2.make( g )
-      def log10 : GE     = Log10.make( g )
-      def sin : GE       = Sin.make( g )
-      def cos : GE       = Cos.make( g )
-      def tan : GE       = Tan.make( g )
-      def asin : GE      = Asin.make( g )
-      def acos : GE      = Acos.make( g )
-      def atan : GE      = Atan.make( g )
-      def sinh : GE      = Sinh.make( g )
-      def cosh : GE      = Cosh.make( g )
-      def tanh : GE      = Tanh.make( g )
-   // def rand : GE              = UnOp.make( 'rand, this )
-   // def rand2 : GE             = UnOp.make( 'rand2, this )
-   // def linrand : GE           = UnOp.make( 'linrand, this )
-   // def bilinrand : GE         = UnOp.make( 'bilinrand, this )
-   // def sum3rand : GE          = UnOp.make( 'sum3rand, this )
-      def distort : GE   = Distort.make( g )
-      def softclip : GE  = Softclip.make( g )
-   // def coin : GE              = UnOp.make( 'coin, this )
-   // def even : GE              = UnOp.make( 'even, this )
-   // def odd : GE               = UnOp.make( 'odd, this )
-   // def rectWindow : GE        = UnOp.make( 'rectWindow, this )
-   // def hanWindow : GE         = UnOp.make( 'hanWindow, this )
-   // def welWindow : GE         = UnOp.make( 'sum3rand, this )
-   // def triWindow : GE         = UnOp.make( 'triWindow, this )
-      def ramp : GE      = Ramp.make( g )
-      def scurve : GE    = Scurve.make( g )
-   // def isPositive : GE        = UnOp.make( 'isPositive, this )
-   // def isNegative : GE        = UnOp.make( 'isNegative, this )
-   // def isStrictlyPositive : GE= UnOp.make( 'isStrictlyPositive, this )
-   // def rho : GE               = UnOp.make( 'rho, this )
-   // def theta : GE             = UnOp.make( 'theta, this )
-
-      import BinaryOp._
-
-      // binary ops
-      private def binOp( op: BinaryOp.Op, b: GE ) : GE =
-         op.make( /* r.getOrElse( this.rate, b.rate ), */ g, b )
-
-      def +( b: GE) = binOp( Plus, b )
-   //      Plus.make /*[ R, S, T ]*/( r.getOrElse( this.rate, b.rate ), this, b )
-
-      def -( b: GE) = binOp( Minus, b )
-
-      def *( b: GE) = binOp( Times, b )
-
-   // def div( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE      = IDiv.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-
-      def / ( b: GE) = binOp( Div, b )
-
-      def % ( b: GE) = binOp( Mod, b )
-
-      def === ( b: GE) = binOp( Eq, b )
-
-      def !== ( b: GE) = binOp( Neq, b )
-
-      def < ( b: GE) = binOp( Lt, b )
-
-      def > ( b: GE) = binOp( Gt, b )
-
-      def <= ( b: GE) = binOp( Leq, b )
-
-      def >= ( b: GE) = binOp( Geq, b )
-
-      def min ( b: GE) = binOp( Min, b )
-
-      def max( b: GE) = binOp( Max, b )
-
-      def & ( b: GE) = binOp( BitAnd, b )
-
-      def | ( b: GE) = binOp( BitOr, b )
-
-      def ^ ( b: GE) = binOp( BitXor, b )
-
-   // def Lcm( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE      = Lcm.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-   // def Gcd( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE      = Gcd.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-
-      def round ( b: GE) = binOp( Round, b )
-
-      def roundup ( b: GE) = binOp( Roundup, b )
-
-      def trunc ( b: GE) = binOp( Trunc, b )
-
-      def atan2 ( b: GE) = binOp( Atan2, b )
-
-      def hypot ( b: GE) = binOp( Hypot, b )
-
-      def hypotx ( b: GE) = binOp( Hypotx, b )
-
-      def pow ( b: GE) = binOp( Pow, b )
-
-   // def <<( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE       = <<.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-   // def >>( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE       = >>.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-   // def unsgnRghtShift( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE = UnsgnRghtShift.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-   // def fill( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE     = Fill.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-
-      def ring1 ( b: GE) = binOp( Ring1, b )
-
-      def ring2 ( b: GE) = binOp( Ring2, b )
-
-      def ring3 ( b: GE) = binOp( Ring3, b )
-
-      def ring4 ( b: GE) = binOp( Ring4, b )
-
-      def difsqr ( b: GE) = binOp( Difsqr, b )
-
-      def sumsqr ( b: GE) = binOp( Sumsqr, b )
-
-      def sqrsum ( b: GE) = binOp( Sqrsum, b )
-
-      def sqrdif ( b: GE) = binOp( Sqrdif, b )
-
-      def absdif ( b: GE) = binOp( Absdif, b )
-
-      def thresh ( b: GE) = binOp( Thresh, b )
-
-      def amclip ( b: GE) = binOp( Amclip, b )
-
-      def scaleneg ( b: GE) = binOp( Scaleneg, b )
-
-      def clip2 ( b: GE) = binOp( Clip2, b )
-
-      def excess ( b: GE) = binOp( Excess, b )
-
-      def fold2 ( b: GE) = binOp( Fold2, b )
-
-      def wrap2 ( b: GE) = binOp( Wrap2, b )
-
-      def firstarg ( b: GE) = binOp( Firstarg, b )
-
-   // def rrand( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE    = Rrand.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-   // def exprrand( b: GE[ /*S,*/ UGenIn /*[ S ]*/])/**/ = : GE = Exprrand.make /*[ R, S, T ]*/( /* r.out,*/ this, b )
-
-//      def linlin( srcLo: GE, srcHi: GE, dstLo: GE, dstHi: GE ) : GE =
-//         LinLin( /* rate, */ g, srcLo, srcHi, dstLo, dstHi )
-//
-//      def linexp( srcLo: GE, srcHi: GE, dstLo: GE, dstHi: GE ) : GE =
-//         LinExp( g.rate, g, srcLo, srcHi, dstLo, dstHi ) // should be highest rate of all inputs? XXX
-   }
 }
 /**
  * The main trait used in synthesis graph, a graph element, abbreviated as `GE`.
--- main/scala/de/sciss/synth/RichNumber.scala
+++ main/scala/de/sciss/synth/RichNumber.scala
@@ -28,8 +28,6 @@ package de.sciss.synth
 import collection.immutable.NumericRange
 
 object RichNumber {
-   private[synth] val LOG2 = math.log( 2 )
-
    /**
     * This would be better `RichFloat.NAryOps`, but scalac can't handle the case
     * op `class RichFloat extends RichFloat.NAryOps` (says it's cyclic) -- damn...
@@ -126,7 +124,7 @@ object RichNumber {
    }
 
    sealed trait NAryDoubleOps {
-      import RichDouble._
+      import DoubleFun._
 
       protected def d: Double
 
@@ -299,7 +297,7 @@ extends RichNumber.UnaryFloatOps with RichNumber.NAryFloatOps with RichNumber.NA
 // ---------------------------- Float ----------------------------
 
 object RichFloat {
-   import RichNumber.LOG2
+   import DoubleFun.LOG2
 
    // -------- unary ops --------
 
@@ -515,186 +513,9 @@ extends RichNumber.UnaryFloatOps with RichNumber.NAryFloatOps with RichNumber.NA
 
 // ---------------------------- Double ----------------------------
 
-object RichDouble {
-   import RichNumber.LOG2
-
-   // -------- unary ops --------
-
-   @inline def rd_neg( d: Double ) : Double	      = -d
-   @inline def rd_abs( d: Double ) : Double	      = math.abs( d )
-   @inline def rd_ceil( d: Double ) : Double       = math.ceil( d )
-   @inline def rd_floor( d: Double ) : Double	   = math.floor( d )
-   @inline def rd_frac( d: Double ) : Double	      = (d - math.floor( d )) // according to jmc
-   @inline def rd_signum( d: Double ) : Double     = math.signum( d )
-   @inline def rd_squared( d: Double ) : Double    = d * d
-   @inline def rd_cubed( d: Double ) : Double      = d * d * d
-   @inline def rd_sqrt( d: Double ) : Double       = math.sqrt( d )
-   @inline def rd_exp( d: Double ) : Double        = math.exp( d )
-   @inline def rd_reciprocal( d: Double ) : Double = 1.0 / d
-   @inline def rd_midicps( d: Double ) : Double    = (440 * math.pow( 2, (d - 69) / 12 ))
-   @inline def rd_cpsmidi( d: Double ) : Double    = (math.log( d / 440 ) / LOG2 * 12 + 69)
-   @inline def rd_midiratio( d: Double ) : Double  = (math.pow( 2, d / 12 ))
-   @inline def rd_ratiomidi( d: Double ) : Double  = (12 * math.log( d ) / LOG2)
-   @inline def rd_dbamp( d: Double ) : Double      = (math.pow( 10, d * 0.05 ))
-   @inline def rd_ampdb( d: Double ) : Double      = (math.log10( d ) * 20)
-   @inline def rd_octcps( d: Double ) : Double     = (440 * math.pow( 2, d - 4.75 ))
-   @inline def rd_cpsoct( d: Double ) : Double     = (math.log( d / 440 ) / LOG2 + 4.75)
-   @inline def rd_log( d: Double ) : Double        = math.log( d )
-   @inline def rd_log2( d: Double ) : Double       = (math.log( d ) / LOG2)
-   @inline def rd_log10( d: Double ) : Double      = math.log10( d )
-   @inline def rd_sin( d: Double ) : Double        = math.sin( d )
-   @inline def rd_cos( d: Double ) : Double        = math.cos( d )
-   @inline def rd_tan( d: Double ) : Double        = math.tan( d )
-   @inline def rd_asin( d: Double ) : Double       = math.asin( d )
-   @inline def rd_acos( d: Double ) : Double       = math.acos( d )
-   @inline def rd_atan( d: Double ) : Double       = math.atan( d )
-   @inline def rd_sinh( d: Double ) : Double       = math.sinh( d )
-   @inline def rd_cosh( d: Double ) : Double       = math.cosh( d )
-   @inline def rd_tanh( d: Double ) : Double       = math.tanh( d )
-//   @inline def rd_distort( d: Double ) : Double    = d / (1 + math.abs( d ))
-//   @inline def rd_softclip( d: Double ) : Double   = { val absx = math.abs( d ); if( absx <= 0.5 ) d else (absx - 0.25) / d}
-//   @inline def rd_ramp( d: Double ) : Double       = if( d <= 0 ) 0 else if( d >= 1 ) 1 else d
-//   @inline def rd_scurve( d: Double ) : Double     = if( d <= 0 ) 0 else if( d > 1 ) 1 else d * d * (3 - 2 * d)
-
-   // ---- binary ops ----
-
-   @inline def rd_+( a: Double, b: Double ) : Double     = a + b
-   @inline def rd_-( a: Double, b: Double ) : Double     = a - b
-   @inline def rd_*( a: Double, b: Double ) : Double     = a * b
-   @inline def rd_div( a: Double, b: Double ) : Int      = (a / b).toInt
-   @inline def rd_/( a: Double, b: Double ) : Double     = a / b
-   @inline def rd_%( a: Double, b: Double ) : Double     = a % b
-//   @inline def rd_===
-//   @inline def rd_!==
-   @inline def rd_<( a: Double, b: Double ) : Boolean    = a < b
-   @inline def rd_>( a: Double, b: Double ) : Boolean    = a > b
-   @inline def rd_<=( a: Double, b: Double ) : Boolean   = a <= b
-   @inline def rd_>=( a: Double, b: Double ) : Boolean   = a >= b
-   @inline def rd_min( a: Double, b: Double ) : Double   = math.min( a, b )
-   @inline def rd_max( a: Double, b: Double ) : Double   = math.max( a, b )
-
-   @inline def rd_round( a: Double, b: Double ) =
-      if( b == 0 ) a else math.floor( a / b + 0.5f ) * b
-
-   @inline def rd_roundup( a: Double, b: Double ) =
-      if( b == 0 ) a else math.ceil( a / b ) * b
-
-   @inline def rd_trunc( a: Double, b: Double ) =
-      if( b == 0 ) a else math.floor( a / b ) * b
-
-   @inline def rd_atan2( a: Double, b: Double ) : Double = math.atan2( a, b )
-   @inline def rd_hypot( a: Double, b: Double ) : Double = math.hypot( a, b )
-
-   @inline def rd_hypotx( a: Double, b: Double ) = {
-      val minab = math.min( math.abs( a ), math.abs( b ))
-      a + b - (math.sqrt(2) - 1) * minab
-   }
-
-   @inline def rd_pow( a: Double, b: Double ) : Double = math.pow( a, b )
-
-// <<
-// >>
-// UnsgnRghtShft
-// fill
-
-   @inline def rd_ring1( a: Double, b: Double ) =
-      a * b + a
-
-   @inline def rd_ring2( a: Double, b: Double ) =
-      a * b + a + b
-
-   @inline def rd_ring3( a: Double, b: Double ) =
-      a * a * b
-
-   @inline def rd_ring4( a: Double, b: Double ) = {
-      val ab = a * b; a * ab - b * ab
-   }
-
-   @inline def rd_difsqr( a: Double, b: Double ) =
-      a * a - b * b
-
-   @inline def rd_sumsqr( a: Double, b: Double ) =
-      a * a + b * b
-
-   @inline def rd_sqrsum( a: Double, b: Double ) = {
-      val z = a + b; z * z
-   }
-
-   @inline def rd_sqrdif( a: Double, b: Double ) = {
-      val z = a - b; z * z
-   }
-
-   @inline def rd_absdif( a: Double, b: Double ) = math.abs( a - b )
-
-   @inline def rd_thresh( a: Double, b: Double ) =
-      if( a < b ) 0 else a
-
-   @inline def rd_amclip( a: Double, b: Double ) =
-      a * 0.5 * (b + math.abs( a ))
-
-   @inline def rd_scaleneg( a: Double, b: Double ) =
-      (math.abs( a ) - a) * (0.5 * b + 0.5) + a
-
-   @inline def rd_clip2( a: Double, b: Double ) =
-      math.max( math.min( a, b ), -b )
-
-   @inline def rd_excess( a: Double, b: Double ) =
-      a - math.max( math.min( a, b ), -b )
-
-   @inline def rd_fold2( a: Double, b: Double ) = rd_fold( a, -b, b )
-
-   @inline def rd_wrap2( a: Double, b: Double ) = rd_wrap( a, -b, b )
-
-   // ---- n-ary ops ----
-
-   @inline def rd_fold( in: Double, lo: Double, hi: Double ) : Double = {
-      val x = in - lo
-      // avoid the divide if possible
-      if( in >= hi ) {
-         val f = hi + hi - in
-         if (f >= lo) return f
-      } else if( in < lo ) {
-         val f = lo + lo - in
-         if( f < hi ) return f
-      } else return in
-
-      if( hi == lo ) return lo
-      // ok do the divide
-      val range   = hi - lo
-      val range2  = range + range
-      val c       = x - range2 * math.floor( x / range2 )
-      lo + (if( c >= range ) range2 - c else c)
-   }
-
-   @inline def rd_wrap( in: Double, lo: Double, hi: Double ) : Double = {
-      // avoid the divide if possible
-      if( in >= hi ) {
-         val range   = hi - lo
-         val in2     = in - range
-         if( in2 < hi ) in2 else if( hi == lo ) lo else {
-            in2 - range * math.floor( (in2 - lo) / range )
-         }
-      } else if( in < lo ) {
-         val range   = hi - lo
-         val in2     = in + range
-         if( in2 >= lo ) in2 else if( hi == lo ) lo else {
-            in2 - range * math.floor( (in2 - lo) / range )
-         }
-      } else in
-   }
-
-   @inline def rd_linlin( in: Double, srcLo: Double, srcHi: Double, dstLo: Double, dstHi: Double ) : Double = {
-      (in - srcLo) / (srcHi - srcLo) * (dstHi - dstLo) + dstLo
-   }
-
-   @inline def rd_linexp( in: Double, srcLo: Double, srcHi: Double, dstLo: Double, dstHi: Double ) : Double = {
-      math.pow( dstHi / dstLo, (in - srcLo) / (srcHi - srcLo)) * dstLo
-   }
-
-}
 final class RichDouble private[synth]( protected val d: Double )
 extends RichNumber.NAryDoubleOps /* with RichNumber.NAryGEOps */ {
-   import RichDouble._
+   import DoubleFun._
 
    protected def cn = Constant( d.toFloat )
 
