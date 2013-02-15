b6c98690c1b3b6005ae0767bef010dfa46aa47cf
Sciss <contact@sciss.de>
2013-02-15 22:02:10 +0100

    before trying to move case class SampleRate() to case object SampleRate

--- api/src/main/scala/de/sciss/synth/Lazy.scala
+++ api/src/main/scala/de/sciss/synth/Lazy.scala
@@ -26,57 +26,57 @@
 package de.sciss.synth
 
 object Lazy {
-   /**
-    * A convenient implementation of the `Lazy` trait for elements which typically expand
-    * to ugens. This will be typically used for elements which do not directly need to
-    * generate ugens but rather spawn more graph elements. For the direct generation of
-    * `UGen`s, use a subtype of `UGenSource`.
-    *
-    * The constructor body of this trait will call `SynthGraph.builder.addLazy` to automatically
-    * register this element when instantiated.
-    *
-    * @tparam U   the type to which this element expands, e.g. `Unit` or `UGenInLike`
-    *
-    * @see [[de.sciss.synth.UGenSource]]
-    */
-   trait Expander[ +U ] extends Lazy /* with MaybeIndividual /* */ with Expands[ U ] */ {
-//      private lazy val cache = new Cache( this )
+  /**
+   * A convenient implementation of the `Lazy` trait for elements which typically expand
+   * to ugens. This will be typically used for elements which do not directly need to
+   * generate ugens but rather spawn more graph elements. For the direct generation of
+   * `UGen`s, use a subtype of `UGenSource`.
+   *
+   * The constructor body of this trait will call `SynthGraph.builder.addLazy` to automatically
+   * register this element when instantiated.
+   *
+   * @tparam U   the type to which this element expands, e.g. `Unit` or `UGenInLike`
+   *
+   * @see [[de.sciss.synth.UGenSource]]
+   */
+  trait Expander[+U] extends Lazy {
+    // this acts now as a fast unique reference
+    @transient private[synth] lazy val ref = new AnyRef
 
-      // this acts now as a fast unique reference
-      @transient private[synth] lazy val ref = new AnyRef
+    // ---- constructor ----
+    SynthGraph.builder.addLazy(this)
 
-      // ---- constructor ----
-      SynthGraph.builder.addLazy( this )
+    /**
+     * A final implementation of this method which calls `visit` on the builder,
+     * checking if this element has already been visited, and if not, will invoke
+     * the `expand` method. Therefore it is guaranteed, that the expansion to
+     * ugens is performed no more than once in the graph expansion.
+     */
+    final private[synth] def force(b: UGenGraph.Builder) {
+      visit(b)
+    }
 
-      /**
-       * A final implementation of this method which calls `visit` on the builder,
-       * checking if this element has already been visited, and if not, will invoke
-       * the `expand` method. Therefore it is guaranteed, that the expansion to
-       * ugens is performed no more than once in the graph expansion.
-       */
-      final private[synth] def force( b: UGenGraph.Builder ) { visit( b )}
+    /**
+     * A final implementation of this method which looks up the current ugen graph
+     * builder and then performs the expansion just as `force`, returning the
+     * expanded object
+     *
+     * @return  the expanded object (e.g. `Unit` for a ugen with no outputs,
+     *          or a single ugen, or a group of ugens)
+     */
+    final private[synth] def expand: U = visit(UGenGraph.builder)
 
-      /**
-       * A final implementation of this method which looks up the current ugen graph
-       * builder and then performs the expansion just as `force`, returning the
-       * expanded object
-       *
-       * @return  the expanded object (e.g. `Unit` for a ugen with no outputs,
-       *          or a single ugen, or a group of ugens)
-       */
-      final private[synth] def expand: U = visit( UGenGraph.builder )
+    private def visit(b: UGenGraph.Builder): U = b.visit(ref, makeUGens)
 
-      private def visit( b: UGenGraph.Builder ): U = b.visit( ref, makeUGens )
-
-      /**
-       * Abstract method which must be implemented by creating the actual `UGen`s
-       * during expansion. This method is at most called once during graph
-       * expansion
-       *
-       * @return  the expanded object (depending on the type parameter `U`)
-       */
-      protected def makeUGens : U
-   }
+    /**
+     * Abstract method which must be implemented by creating the actual `UGen`s
+     * during expansion. This method is at most called once during graph
+     * expansion
+     *
+     * @return  the expanded object (depending on the type parameter `U`)
+     */
+    protected def makeUGens: U
+  }
 }
 
 /**
@@ -94,12 +94,12 @@ object Lazy {
  * @see [[de.sciss.synth.Lazy.Expander]]
  */
 trait Lazy extends Serializable {
-   /**
-    * This method is invoked by the `UGenGraphBuilder` instance when a `SynthGraph`
-    * is expanded.
-    *
-    * @param b    the ugen graph builder to which expanded `UGen`s or control proxies
-    *             may be added.
-    */
-   private[synth] def force( b: UGenGraph.Builder ) : Unit
+  /**
+   * This method is invoked by the `UGenGraphBuilder` instance when a `SynthGraph`
+   * is expanded.
+   *
+   * @param b    the ugen graph builder to which expanded `UGen`s or control proxies
+   *             may be added.
+   */
+  private[synth] def force(b: UGenGraph.Builder): Unit
 }
