b9a1f31f59858e3cd859234f2e447920566c2977
Sciss <contact@sciss.de>
2013-06-02 00:20:40 +0200

    fix klang spec product prefix

--- core/src/main/scala/de/sciss/synth/ugen/KlangSpec.scala
+++ core/src/main/scala/de/sciss/synth/ugen/KlangSpec.scala
@@ -11,7 +11,7 @@ object KlangSpec {
     new Seq(IIdxSeq.tabulate[(GE, GE, GE)](n)(func ).map((tup) => KlangSpec(tup._1, tup._2, tup._3)))
 
   final case class Seq(elems: IIdxSeq[KlangSpec]) extends GE {
-    override def productPrefix  = "KlangSpec.Seq"
+    override def productPrefix  = "KlangSpec$Seq"
     def numOutputs              = elems.size * 3
     def rate                    = MaybeRate.reduce(elems.map(_.rate): _*)
     override def toString       = productPrefix + elems.mkString("(", ",", ")")
